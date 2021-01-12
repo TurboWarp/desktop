7514ac54e5bd10a213a5376225f9149b754d6777
Thomas Weber <weber_t@mailbox.org>
2021-01-11 20:49:08 -0600

    Import color-picker addon

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,6 +1,9 @@
 const searchParams = new URLSearchParams(location.search);
 const route = searchParams.get('route');
 
+// Trick some libraries into thinking they're not running in node
+window.module = undefined;
+
 if (route === 'editor') {
   import('./gui/gui.jsx');
 } else if (route === 'about') {
