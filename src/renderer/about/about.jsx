b6f23d75c453ad50700ebb84353bc7529d8be5c6
Thomas Weber <weber_t@mailbox.org>
2021-02-05 22:43:04 -0600

    Include node version in about

--- src/renderer/about/about.jsx
+++ src/renderer/about/about.jsx
@@ -10,7 +10,7 @@ ReactDOM.render((
     <h1>TurboWarp Desktop v{version}</h1>
     <p>
       <a href="https://desktop.turbowarp.org" target="_blank" rel="noreferrer">https://desktop.turbowarp.org</a>
-      &nbsp;Electron {process.versions.electron}, Chrome {process.versions.chrome}
+      &nbsp;Electron {process.versions.electron}, Chrome {process.versions.chrome}, Node {process.versions.node}
     </p>
     <h2>License</h2>
     <pre>{licenseText}</pre>
