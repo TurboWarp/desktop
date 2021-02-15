f6e92e160144a5d0c1a8cce919a6ebedf7971348
Thomas Weber <weber_t@mailbox.org>
2021-02-14 19:42:15 -0600

    Update website privacy policy to match in-app

--- src/renderer/privacy/privacy.jsx
+++ src/renderer/privacy/privacy.jsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import styles from './privacy.css';
 
 ReactDOM.render((
-  // TODO: Update privacy.html to match, perhaps generate this page from privacy.html?
+  // Please make sure privacy.html is always the same as this.
   <main>
     <h1>Privacy Policy</h1>
-    <p><i>Updated February 13th, 2021</i></p>
+    <p><i>Updated February 14th, 2021</i></p>
     <p>The app does not collect any information about you or your use of the app.</p>
     <p>The app may make requests to check for updates. These requests are not logged. This is an important security feature that cannot be disabled.</p>
     <p>Any concerns related to privacy or any other matter should be sent to: <a href="mailto:contact@turbowarp.org">contact@turbowarp.org</a></p>
