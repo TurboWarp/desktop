4f6842a0fe7e1c845ab74f8ebaee3d900bc7222a
Thomas Weber <weber_t@mailbox.org>
2021-03-04 18:41:16 -0600

    Remove unnecessary "License" header

--- src/renderer/about/about.jsx
+++ src/renderer/about/about.jsx
@@ -9,7 +9,6 @@ ReactDOM.render((
   <main>
     <h1>TurboWarp Desktop v{version}</h1>
     <p>TurboWarp is a mod of Scratch with a compiler and more features. TurboWarp is not affiliated with Scratch, the Scratch Team, or the Scratch Foundation. Learn more at <a href="https://desktop.turbowarp.org" target="_blank" rel="noreferrer">https://desktop.turbowarp.org</a>.</p>
-    <h2>License</h2>
     <p>TurboWarp Desktop is licensed under the GNU General Public License v3.0. The source code is published <a href="https://github.com/TurboWarp/" target="_blank" rel="noreferrer">on GitHub</a>.</p>
     <pre>{licenseText}</pre>
   </main>
