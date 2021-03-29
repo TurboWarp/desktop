cb9a9334485109f4eb43de5546032fe6b634ada3
Thomas Weber <weber_t@mailbox.org>
2021-03-28 19:53:29 -0500

    Revert "Enable sandbox"
    This reverts commit 55285d39d7cd1f788b5160d28387432595e97667.

--- src/main/index.js
+++ src/main/index.js
@@ -84,8 +84,6 @@ if (isMac) {
   Menu.setApplicationMenu(null);
 }
 
-app.enableSandbox();
-
 function getURL(route) {
   if (isDevelopment) {
     return `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}/?route=${route}`;
