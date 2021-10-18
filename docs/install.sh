075f8cc5dba24015ea7b2bf1bf26330f6dbd4f93
Thomas Weber <weber_t@mailbox.org>
2021-10-18 17:01:09 -0500

    0.11.0 is released

--- docs/install.sh
+++ docs/install.sh
@@ -24,7 +24,7 @@ if [ "$(whoami)" != "root" ]; then
     fatal "Must be run as root."
 fi
 
-VERSION="0.10.1"
+VERSION="0.11.0"
 ARCH="$(uname -m)"
 echo "Version: $VERSION"
 echo "System archictecture: $ARCH"
