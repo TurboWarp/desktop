95fb79ebede5113ec5f03f0360cab894e80939a6
Thomas Weber <weber_t@mailbox.org>
2021-11-18 20:58:23 -0600

    Update website for v0.12.0

--- docs/install.sh
+++ docs/install.sh
@@ -24,7 +24,7 @@ if [ "$(whoami)" != "root" ]; then
     fatal "Must be run as root."
 fi
 
-VERSION="0.11.0"
+VERSION="0.12.0"
 ARCH="$(uname -m)"
 echo "Version: $VERSION"
 echo "System archictecture: $ARCH"
