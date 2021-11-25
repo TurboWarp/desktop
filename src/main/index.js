f8a900debf825a51b4b5530fd54061a9903f1d29
Thomas Weber <weber_t@mailbox.org>
2021-11-25 15:06:59 -0600

    Remove userAgentFallback override
    Our scratch-vm will soon not include user agents in projects at all, so we don't need to touch the user agent

--- src/main/index.js
+++ src/main/index.js
@@ -642,14 +642,6 @@ app.on('web-contents-created', (event, webContents) => {
   });
 });
 
-// We might have a user agent like:
-// Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) turbowarp-desktop/0.8.0 Chrome/91.0.4472.124 Electron/13.1.7 Safari/537.36
-// We want to remove the Electron/ and turbowarp-desktop/ parts.
-app.userAgentFallback = app.userAgentFallback
-  .replace(/Electron\/[0-9.]+/, '')
-  .replace(`${manifest.name}/${manifest.version}`, '')
-  .replace(/ {2,}/g, ' '); 
-
 const acquiredLock = app.requestSingleInstanceLock();
 if (acquiredLock) {
   const autoCreateEditorWindows = () => {
