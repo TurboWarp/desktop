96268b9bdb244b7423b0031daec979a236cc728c
Thomas Weber <muffin@muffin.ink>
2021-12-19 11:22:58 -0600

    Refactor packager download script

--- scripts/download-packager.js
+++ scripts/download-packager.js
@@ -2,35 +2,33 @@ const fs = require('fs');
 const crypto = require('crypto');
 const pathUtil = require('path');
 const {fetch} = require('./lib');
-
-const DOWNLOAD_URL = 'https://github.com/TurboWarp/packager/releases/download/v0.3.0/turbowarp-packager-standalone-0.3.0.html';
-const CHECKSUM = '1b850c944e5a852c81a6132e60af2887065675d5ae8fa2380a3498b562898116';
+const packagerInfo = require('./packager.json');
 
 const path = pathUtil.join(__dirname, '..', 'static', 'packager.html');
 const sha256 = (buffer) => crypto.createHash('sha256').update(buffer).digest('hex');
 
-const needToFetch = () => {
+const isAlreadyDownloaded = () => {
   try {
-    if (sha256(fs.readFileSync(path)) === CHECKSUM) {
-      return false;
+    if (sha256(fs.readFileSync(path)) === packagerInfo.sha256) {
+      return true;
     }
   } catch (e) {
     // file might not exist, ignore
   }
-  return true;
+  return false;
 };
 
 
-if (needToFetch()) {
-  console.log(`Downloading ${DOWNLOAD_URL}`);
+if (!isAlreadyDownloaded()) {
+  console.log(`Downloading ${packagerInfo.src}`);
   console.time('Download packager');
 
-  fetch(DOWNLOAD_URL)
+  fetch(packagerInfo.src)
     .then((res) => res.buffer())
     .then((buffer) => {
       const sha256 = crypto.createHash('sha256').update(buffer).digest('hex');
-      if (CHECKSUM !== sha256) {
-        throw new Error(`Hash mismatch: expected ${CHECKSUM} but found ${sha256}`);
+      if (packagerInfo.sha256 !== sha256) {
+        throw new Error(`Hash mismatch: expected ${packagerInfo.sha256} but found ${sha256}`);
       }
       fs.writeFileSync(pathUtil.join(__dirname, '..', 'static', 'packager.html'), buffer);
       console.timeEnd('Download packager');
