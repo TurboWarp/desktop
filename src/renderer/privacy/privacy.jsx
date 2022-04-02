2693a321cfa0123b6f89e3c26c8499c4838755c0
Thomas Weber <muffin@muffin.ink>
2022-04-02 14:55:57 -0500

    Update in-app privacy policy when update checker is disabled at build-time

--- src/renderer/privacy/privacy.jsx
+++ src/renderer/privacy/privacy.jsx
@@ -9,13 +9,19 @@ const openDesktopSettings = (e) => {
   ipcRenderer.send('open-desktop-settings');
 };
 
+const canUpdatesBeEnabled = ipcRenderer.sendSync('update-checker/can-be-enabled');
+
 ReactDOM.render((
   // Please make sure privacy.html is always the same as this.
   <main>
     <h1>Privacy Policy</h1>
     <p><i>Updated December 8th, 2021</i></p>
 
-    <p>The app may make requests to check for updates. These requests are not logged. These requests <a onClick={openDesktopSettings}>can be disabled</a>.</p>
+    {canUpdatesBeEnabled ? (
+      <p>The app may make requests to check for updates. These requests are not logged. These requests <a onClick={openDesktopSettings}>can be disabled</a>.</p>
+    ) : (
+      <p>The builtin update checker is disabled in this build of the app.</p>
+    )}
     <p>Scratch extensions that require Wi-Fi (such as Translate, Text to Speech, LEGO, micro:bit, etc.) may connect to the Scratch API to implement these features. <a href="https://scratch.mit.edu/privacy_policy/" target="_blank">Refer to the Scratch privacy policy for more information</a>. The Translate extension may instead make requests to a TurboWarp API, which may then forward your request to the Scratch API and log the message being translated and the result for caching and performance.</p>
 
     <h2>Packager</h2>
