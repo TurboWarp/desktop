1ce53b876c22575f4a9409339b1dd11e33caa01b
Thomas Weber <muffin@muffin.ink>
2022-04-02 20:16:02 -0500

    Update render process crash message

--- src/main/crash.js
+++ src/main/crash.js
@@ -1,10 +1,14 @@
 import {app, dialog, BrowserWindow} from 'electron';
 import {getTranslation} from './translations';
+import {APP_NAME} from './brand';
 
 app.on('render-process-gone', (event, webContents, details) => {
   const window = BrowserWindow.fromWebContents(webContents);
   dialog.showMessageBoxSync(window, {
-    message: getTranslation('crash.renderer')
+    type: 'error',
+    title: APP_NAME,
+    message: getTranslation('crash.title'),
+    detail: getTranslation('crash.renderer')
       .replace('{code}', details.exitCode)
       .replace('{reason}', details.reason)
   });
