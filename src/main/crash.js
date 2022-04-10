512fa919251df9c4791b223ee56069c2bca9b76a
Thomas Weber <muffin@muffin.ink>
2022-04-10 11:44:55 -0500

    Show error when any child process crashes, not just renderers

--- src/main/crash.js
+++ src/main/crash.js
@@ -2,14 +2,23 @@ import {app, dialog, BrowserWindow} from 'electron';
 import {getTranslation} from './translations';
 import {APP_NAME} from './brand';
 
-app.on('render-process-gone', (event, webContents, details) => {
-  const window = BrowserWindow.fromWebContents(webContents);
+const showCrashMessage = (window, type, code, reason) => {
   dialog.showMessageBoxSync(window, {
     type: 'error',
     title: APP_NAME,
     message: getTranslation('crash.title'),
-    detail: getTranslation('crash.renderer')
-      .replace('{code}', details.exitCode)
-      .replace('{reason}', details.reason)
+    detail: getTranslation('crash.description')
+      .replace('{type}', type)
+      .replace('{code}', code)
+      .replace('{reason}', reason)
   });
+};
+
+app.on('render-process-gone', (event, webContents, details) => {
+  const window = BrowserWindow.fromWebContents(webContents);
+  showCrashMessage(window, 'Renderer', details.exitCode, details.reason);
+});
+
+app.on('child-process-gone', (event, details) => {
+  showCrashMessage(null, details.type, details.exitCode, details.reason);
 });
