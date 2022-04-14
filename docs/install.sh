666f6e55a3afdb52e2e26c79b26183adb55b12cb
Thomas Weber <muffin@muffin.ink>
2022-04-13 21:50:13 -0500

    1.2.2 is released

--- docs/install.sh
+++ docs/install.sh
@@ -32,7 +32,7 @@ if [ "$(whoami)" != "root" ]; then
     fatal "Must be run as root."
 fi
 
-VERSION="1.2.1"
+VERSION="1.2.2"
 ARCH="$(uname -m)"
 echo "Version: $VERSION"
 echo "System archictecture: $ARCH"
