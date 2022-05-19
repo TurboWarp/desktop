e38cca9710d894b8f362c61d5b37626ba961c960
Thomas Weber <muffin@muffin.ink>
2022-05-18 19:02:58 -0500

    1.3.1 is released

--- docs/install.sh
+++ docs/install.sh
@@ -32,7 +32,7 @@ if [ "$(whoami)" != "root" ]; then
     fatal "Must be run as root."
 fi
 
-VERSION="1.3.0"
+VERSION="1.3.1"
 ARCH="$(uname -m)"
 echo "Version: $VERSION"
 echo "System archictecture: $ARCH"
