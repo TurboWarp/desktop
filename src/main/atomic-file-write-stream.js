c2137f8f40f0e737deb162f12ee586aaaf60a288
GarboMuffin <muffin@muffin.ink>
2023-01-06 01:33:48 -0600

    Add more error handling to atomic file write

--- src/main/atomic-file-write-stream.js
+++ src/main/atomic-file-write-stream.js
@@ -1,43 +1,82 @@
 import fs from 'fs';
-import MurmurHash3 from 'imurmurhash';
 import {promisify} from 'util';
 
 // This file was initially based on:
 // https://github.com/npm/write-file-atomic/blob/a37fdc843f4d391cf1cff85c8e69c3d80e05b049/lib/index.js
 
-let invocations = 0
 const getTemporaryPath = (originalPath) => {
-  invocations += 1;
   // The temporary file needs to be located on the same physical disk as the actual file,
   // otherwise we won't be able to rename it.
-  return originalPath + '.' + MurmurHash3()
-    .hash(__filename)
-    .hash(String(process.pid))
-    .hash(String(invocations))
-    .result();
+  let random = '';
+  for (let i = 0; i < 4; i++) {
+    random += Math.floor(Math.random() * 10).toString();
+  }
+  return originalPath + '.tw' + random;
+};
+
+const getOriginalMode = async (path) => {
+  try {
+    const stat = await promisify(fs.stat)(path);
+    return stat.mode;
+  } catch (e) {
+    // read and write for all users
+    return 0o666;
+  }
 };
 
 const createAtomicWriteStream = async (path) => {
-  // TODO: error handling
-  // TODO: preserve mode
+  const originalMode = await getOriginalMode(path);
 
   const tempPath = getTemporaryPath(path);
-  const fd = await promisify(fs.open)(tempPath, 'w');
+  const fd = await promisify(fs.open)(tempPath, 'wx', originalMode);
   const writeStream = fs.createWriteStream(null, {
     fd,
     autoClose: false
   });
 
-  const write = (data) => writeStream.write(data);
-
-  const finish = async () => {
-    await promisify(writeStream.end.bind(writeStream))();
-    await promisify(fs.fsync)(fd);
-    await promisify(fs.rename)(tempPath, path);
-    await promisify(fs.close)(fd);
+  const cleanAfterError = async () => {
+    try {
+      await promisify(fs.close)(fd);
+    } catch (e) {
+      // ignore
+    }
+    try {
+      await promisify(fs.unlink)(tempPath);
+    } catch (e) {
+      // ignore
+    }
   };
 
-  const drain = () => new Promise((resolve) => {
+  let error = null;
+  writeStream.on('error', (newError) => {
+    if (!error) {
+      error = newError;
+      console.error(error);
+      cleanAfterError();
+    }
+  });
+
+  const write = (data) => {
+    if (error) {
+      throw error;
+    }
+    return writeStream.write(data);
+  };
+
+  const finish = async () => {
+    if (error) {
+      throw error;
+    }
+    await promisify(writeStream.end.bind(writeStream))();
+    await promisify(fs.fsync)(fd);
+    await promisify(fs.close)(fd);
+    await promisify(fs.rename)(tempPath, path);
+  };
+
+  const drain = () => new Promise((resolve, reject) => {
+    if (error) {
+      return reject(error);
+    }
     writeStream.once('drain', () => {
       resolve();
     });
