d121ae3442430cbfbb864f07d2901ecfa7df93f5
GarboMuffin <muffin@mailbox.org>
2023-06-19 18:50:28 -0500

    Release 1.8.0

--- docs/install.sh
+++ docs/install.sh
@@ -35,7 +35,7 @@ if [ "$(whoami)" != "root" ]; then
     fatal "Must be run as root."
 fi
 
-VERSION="1.7.1"
+VERSION="1.8.0"
 ARCH="$(uname -m)"
 echo "Version: $VERSION"
 echo "System archictecture: $ARCH"
