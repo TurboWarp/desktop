aa5ee97a3e060c3ebfd48125fc006017ec5f5139
GarboMuffin <muffin@mailbox.org>
2023-06-19 20:14:02 -0500

    Update debian repository upload script again

--- debian/upload.sh
+++ debian/upload.sh
@@ -2,4 +2,10 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-rclone copy deb r2:turbowarp-pkgs/deb -P --transfers=1
+# Order matters. We want to make sure all the .deb files in pool are uploaded before
+# we upload the new release files in dists, otherwise there can be a long period where
+# apt is broken.
+# --transfers=1 significantly improves reliability in our experience on both good and
+# bad internet connections.
+rclone copy deb/pool r2:turbowarp-pkgs/deb/pool -P --transfers=1
+rclone copy deb/dists r2:turbowarp-pkgs/deb/dists -P --transfers=1
