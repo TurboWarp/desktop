4fbc3134806aa2e43ffe3e65e53fe95f16d77b13
Muffin <muffin@mailbox.org>
2023-08-19 18:24:33 -0500

    Delete old-src

--- old-src/main/environment.js
+++
@@ -1,7 +0,0 @@
-// import path from 'path';
-
-// export const isDevelopment = process.env.NODE_ENV !== 'production';
-// export const isMac = process.platform === 'darwin';
-// export const isLinux = process.platform === 'linux';
-// // __static is unreliable when started with electron.exe main.js ...
-// export const staticDir = isDevelopment ? path.join(__dirname, '../../static') : path.join(__dirname, '../static');
