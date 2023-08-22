fb9be2bfda9fb28aea4df9d25f71cb973100e132
Muffin <muffin@mailbox.org>
2023-08-21 19:30:03 -0500

    Add a bit more to the privacy policy

--- src-main/windows/privacy.js
+++ src-main/windows/privacy.js
@@ -1,10 +1,18 @@
 const BaseWindow = require('./base');
+const DesktopSettingsWindow = require('./desktop-settings');
 const {translate} = require('../l10n');
 const {APP_NAME} = require('../brand');
 
 class PrivacyWindow extends BaseWindow {
   constructor () {
     super();
+
+    const ipc = this.window.webContents.ipc;
+
+    ipc.handle('open-desktop-settings', () => {
+      DesktopSettingsWindow.show();
+    });
+
     this.window.setTitle(`${translate('privacy-policy')} - ${APP_NAME}`);
     this.window.setMinimizable(false);
     this.window.setMaximizable(false);
@@ -16,6 +24,10 @@ class PrivacyWindow extends BaseWindow {
       width: 800,
       height: 700
     };
+  }
+
+  getPreload () {
+    return 'privacy';
   }
 
   isPopup () {
