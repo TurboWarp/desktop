13f121558e20236d18004219c699c619ad4f6148
GarboMuffin <muffin@mailbox.org>
2023-09-07 12:17:09 -0500

    Show error message for unhandled promise rejections

--- src-main/entrypoint.js
+++ src-main/entrypoint.js
@@ -1,15 +1,32 @@
-try {
-  require('./index');
-} catch (error) {
-  // It's very important that this code here be minimal and self-contained so
-  // that we don't have an error in the error handling code.
-  console.error(error);
-  const {app, dialog} = require('electron');
+// It's very important that all error-handling code here be as minimal and
+// self contained as possible  to ensure that the error handling does not
+// itself have errors.
+
+const {app, dialog} = require('electron');
+
+const APP_NAME = 'TurboWarp Desktop';
+const stringifyError = (error) => (error && error.stack) ? error.stack : error;
+
+process.on('unhandledRejection', (error) => {
+  console.error('Error in promise:', error);
   app.whenReady().then(() => {
     dialog.showMessageBoxSync({
       type: 'error',
-      title: 'Error',
-      message: `Error starting main process:\n\n${(error && error.stack) ? error.stack : error}`
+      title: APP_NAME,
+      message: `Error in promise: ${stringifyError(error)}`
+    });  
+  });
+});
+
+try {
+  require('./index');
+} catch (error) {
+  console.error('Error starting main process:', error);
+  app.whenReady().then(() => {
+    dialog.showMessageBoxSync({
+      type: 'error',
+      title: APP_NAME,
+      message: `Error starting main process: ${stringifyError(error)}`
     });
     app.exit(1);
   });
