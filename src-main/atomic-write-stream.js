9676e92feb0dfd9d1491127fcfb376116a6bc142
GarboMuffin <muffin@mailbox.org>
2023-09-28 01:20:36 -0500

    Implement partial support for atomic writes on macOS
    We write the atomic data to temp, then rename
    In a vast majority of cases, the temp directory and the destination file
    will be on the same drive, so this usually works perfectly.
    In other cases we will unfortunately have to do another file copy
    which is very not ideal.
    
    I suppose theoretically the EXDEV case could've happened before on
    very weird and exotic systems...?

--- src-main/atomic-write-stream.js
+++ src-main/atomic-write-stream.js
@@ -1,18 +1,32 @@
 const fs = require('fs');
 const fsPromises = require('fs/promises');
 const nodeCrypto = require('crypto');
+const pathUtil = require('path');
+const {app} = require('electron');
 
 // This file was initially based on:
 // https://github.com/npm/write-file-atomic/blob/a37fdc843f4d391cf1cff85c8e69c3d80e05b049/lib/index.js
 
-const getTemporaryPath = (originalPath) => {
-  // The temporary file needs to be located on the same physical disk as the actual file,
-  // otherwise we won't be able to rename it.
-  let random = '';
-  for (let i = 0; i < 4; i++) {
-    random += Math.floor(Math.random() * 10).toString();
+/**
+ * @param {string} originalPath
+ * @param {boolean} mustUseTempDir
+ * @returns {string}
+ */
+const getTemporaryPath = (originalPath, mustUseTempDir) => {
+  const randomNumbers = Math.floor(Math.random() * 10000)
+    .toString()
+    .padStart(4, '0');
+  const randomSuffix = `.tw${randomNumbers}`;
+
+  // Ideally the temporary file and destination file should be located on the
+  // same drive and partition.
+  if (mustUseTempDir) {
+    const tempDir = app.getPath('temp');
+    const basename = pathUtil.basename(originalPath);
+    return pathUtil.join(tempDir, `${basename}${randomSuffix}`);
+  } else {
+    return `${originalPath}${randomSuffix}`;
   }
-  return originalPath + '.tw' + random;
 };
 
 const getOriginalMode = async (path) => {
@@ -94,17 +108,39 @@ const areSameFile = async (a, b) => {
   }
 };
 
+/**
+ * @param {string} from
+ * @param {string} to
+ * @returns {Promise<void>}
+ */
+const copy = (from, to) => new Promise((resolve, reject) => {
+  // fs.copyFile's error handling does more than we want.
+  // On error we want to leave the destination file to allow possible
+  // data recovery later.
+  const readStream = fs.createReadStream(from);
+  const writeStream = fs.createWriteStream(to);
+  readStream.on('error', (error) => {
+    reject(error);
+  });
+  writeStream.on('error', (error) => {
+    reject(error);
+  });
+  writeStream.on('finish', () => {
+    resolve();
+  });
+  readStream.pipe(writeStream);
+});
+
 const createAtomicWriteStream = async (path) => {
   const releaseFileLock = await acquireFileLock(path);
 
   const originalMode = await getOriginalMode(path);
 
-  // Mac App Store sandbox is *very* restrictive so the atomic writing with a
-  // temporary file won't work :(
-  // Here we still prevent concurrent writes, at least, but not atomic
-  const atomicSupported = !process.mas;
+  // Mac App Store sandbox prevents us from saving the temporary file in the
+  // same directory as the destination file.
+  const isSeverelySandboxed = !!process.mas;
 
-  const tempPath = atomicSupported ? getTemporaryPath(path) : path;
+  const tempPath = getTemporaryPath(path, isSeverelySandboxed);
   const fileHandle = await fsPromises.open(tempPath, 'w', originalMode);
   const writeStream = fileHandle.createWriteStream({
     autoClose: false,
@@ -121,15 +157,13 @@ const createAtomicWriteStream = async (path) => {
       });
     });
 
-    if (atomicSupported) {
-      try {
-        // TODO: it might make sense to leave the broken file on the disk so that
-        // there is a chance of recovery?
-        await fsPromises.unlink(tempPath);
-      } catch (e) {
-        // ignore; file might have been removed already or was never successfully
-        // created
-      }
+    try {
+      // TODO: it might make sense to leave the broken file on the disk so that
+      // there is a chance of recovery?
+      await fsPromises.unlink(tempPath);
+    } catch (e) {
+      // ignore; file might have been removed already or was never successfully
+      // created
     }
 
     writeStream.emit('atomic-error', error);
@@ -155,23 +189,35 @@ const createAtomicWriteStream = async (path) => {
         });
       });
 
-      if (atomicSupported) {
-        try {
-          await fsPromises.rename(tempPath, path);
-        } catch (err) {
+      try {
+        await fsPromises.rename(tempPath, path);
+      } catch (err) {
+        if (err.code === 'EXDEV') {
+          // The temporary file and the destination file were located on separate
+          // drives or partitions, so we need to copy instead. This is not ideal
+          // and is not atomic, but:
+          //  - This is a relatively rare edge case
+          //  - Much of the file saving process is still safe to abort at any time
+          //    (Pure IO should be faster than zipping the project)
+          //  - We still avoid keeping the entire file in memory at once
+          await copy(tempPath, path);
+          await fsPromises.unlink(tempPath);
+
+          const destinationHandle = await fsPromises.open(path, 'r');
+          await destinationHandle.sync();
+          await destinationHandle.close();
+        } else if (
           // On Windows, the rename can fail with EPERM even though it succeeded.
           // https://github.com/npm/fs-write-stream-atomic/commit/2f51136f24aaefebd446455a45fa108909b18ca9
-          if (
-            process.platform === 'win32' &&
-            err.syscall === 'rename' &&
-            err.code === 'EPERM' &&
-            await areSameFile(path, tempPath)
-          ) {
-            // The rename did actually succeed, so we can remove the temporary file
-            await fsPromises.unlink(tempPath);
-          } else {
-            throw err;
-          }
+          process.platform === 'win32' &&
+          err.syscall === 'rename' &&
+          err.code === 'EPERM' &&
+          await areSameFile(path, tempPath)
+        ) {
+          // The rename did actually succeed, so we can remove the temporary file
+          await fsPromises.unlink(tempPath);
+        } else {
+          throw err;
         }
       }
 
