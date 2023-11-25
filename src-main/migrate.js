65e76d8e99b0c20c2b077ffcc4d786b04fea4ab8
Muffin <muffin@mailbox.org>
2023-11-25 17:17:06 -0600

    Warn when someone downgrades across dataVersion boundaries

--- src-main/migrate.js
+++ src-main/migrate.js
@@ -1,8 +1,11 @@
-const {app} = require('electron');
+const {app, dialog} = require('electron');
 const fs = require('fs');
 const path = require('path');
 const settings = require('./settings');
 const MigrateWindow = require('./windows/migrate');
+const {APP_NAME} = require('./brand');
+const {translate} = require('./l10n');
+const safelyOpenExternal = require('./open-external');
 
 // Avoid running migrate logic on fresh installs when we can. Not required, just helps
 // user experience. This must run before the ready event.
@@ -14,6 +17,32 @@ const isFirstLaunch = (
 );
 
 const migrate = async () => {
+  if (settings.dataVersion > MigrateWindow.LATEST_VERSION) {
+    const result = dialog.showMessageBoxSync({
+      type: 'error',
+      title: APP_NAME,
+      message: translate('migrate-future.message')
+        .replace('{APP_NAME}', APP_NAME)
+        .replace('{website}', 'desktop.turbowarp.org')
+        .replace('{debugInfo}', `${MigrateWindow.LATEST_VERSION} < ${settings.dataVersion}`),
+      buttons: [
+        translate('migrate-future.exit'),
+        translate('migrate-future.continue-anyways')
+      ],
+      cancelId: 0,
+      defaultId: 0
+    });
+    if (result === 0) {
+      safelyOpenExternal('https://desktop.turbowarp.org/');
+      app.exit(1);
+    } else {
+      // Hope for the best!
+      settings.dataVersion = MigrateWindow.LATEST_VERSION;
+      await settings.save();
+      return;
+    }
+  }
+
   if (settings.dataVersion === MigrateWindow.LATEST_VERSION) {
     return;
   }
