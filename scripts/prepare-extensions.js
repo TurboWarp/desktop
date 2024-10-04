6833a862e642a0c99be6922c48b69cfaaa71578c
FurryR <awathefox@gmail.com>
2024-10-04 14:10:34 +0800

    asyncify
    Signed-off-by: FurryR <awathefox@gmail.com>

--- scripts/prepare-extensions.js
+++ scripts/prepare-extensions.js
@@ -14,8 +14,8 @@ const pathUtil = require('path');
   const outputDirectory = pathUtil.join(__dirname, '../dist-extensions/');
   const mode = 'desktop';
   const builder = new Builder(mode);
-  const build = builder.build();
-  build.export(outputDirectory);
+  const build = await builder.build();
+  await build.export(outputDirectory);
 
   console.log(`Built ${mode} copy of extensions.turbowarp.org to ${outputDirectory}`);
 })();
