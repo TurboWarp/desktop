3d822c67469526ab64a38969b45441afe26f6d9c
FurryR <awathefox@gmail.com>
2024-10-04 14:08:58 +0800

    ESM-ify extensions
    Signed-off-by: FurryR <awathefox@gmail.com>

--- scripts/prepare-extensions.js
+++ scripts/prepare-extensions.js
@@ -1,19 +1,21 @@
 const pathUtil = require('path');
 
-let Builder;
-try {
-  Builder = require('../extensions/development/builder');
-} catch (e) {
-  console.error('Could not load TurboWarp/extensions build scripts, most likely because the submodule is missing.');
-  console.error('Try running: `git submodule init` and `git submodule update`');
-  console.error(e);
-  process.exit(1);
-}
+(async () => {
+  let Builder;
+  try {
+    Builder = await import('../extensions/development/builder');
+  } catch (e) {
+    console.error('Could not load TurboWarp/extensions build scripts, most likely because the submodule is missing.');
+    console.error('Try running: `git submodule init` and `git submodule update`');
+    console.error(e);
+    process.exit(1);
+  }
 
-const outputDirectory = pathUtil.join(__dirname, '../dist-extensions/');
-const mode = 'desktop';
-const builder = new Builder(mode);
-const build = builder.build();
-build.export(outputDirectory);
+  const outputDirectory = pathUtil.join(__dirname, '../dist-extensions/');
+  const mode = 'desktop';
+  const builder = new Builder(mode);
+  const build = builder.build();
+  build.export(outputDirectory);
 
-console.log(`Built ${mode} copy of extensions.turbowarp.org to ${outputDirectory}`);
+  console.log(`Built ${mode} copy of extensions.turbowarp.org to ${outputDirectory}`);
+})();
