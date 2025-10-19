08410d30d94d8aa4c0308ed07367741ffa8709cc
Thomas Weber <muffin@muffin.ink>
2025-10-19 11:40:55 -0500

    Update outdated comment

--- src-main/index.js
+++ src-main/index.js
@@ -27,11 +27,11 @@ app.commandLine.appendSwitch('host-resolver-rules', 'MAP device-manager.scratch.
 if (!settings.hardwareAcceleration) {
   app.disableHardwareAcceleration();
 
-  // SwiftShader is Chromium's software WebGL fallback. Starting in Chrome 137,
-  // it will be disabled by default. Enabling SwiftShader is required for the
-  // editor to work without hardware acceleration, so adding this flag will be
-  // required. Chromium considers this dangerous, so only add the flag when it
-  // is needed.
+  // SwiftShader is Chromium's software WebGL fallback. Starting in Chrome 139,
+  // it is disabled by default. Enabling SwiftShader is required for the editor
+  // to work without hardware acceleration, so adding this flag will be
+  // required. Google considers this dangerous, so only add the flag when it is
+  // needed.
   // https://github.com/TurboWarp/desktop/issues/1158
   // https://chromestatus.com/feature/5166674414927872
   // https://chromium.googlesource.com/chromium/src/+/main/docs/gpu/swiftshader.md
